e722864348de15afe41cbf1da4eceefdd899a886
github username <github email address>
2020-11-09 18:09:51 -0500

    Added Flower Show Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #GET /api/v1/flowers/:id
+  namespace :api do
+    namespace :v1 do
+      resources :flowers, only: [:show]
+    end
+  end
+
 end
