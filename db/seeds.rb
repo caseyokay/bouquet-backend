3d9b22122e84583ab35db1b38cc593ec74a08785
github username <github email address>
2020-11-09 17:39:47 -0500

    Added Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts "Deleting old records"
+# Flower.destroy_all
+
+puts "creating new records"
+
+10.times do
+    flower = Flower.create!(
+        name: Faker::Artist.name,
+        image_url: "https://cdn.shopify.com/s/files/1/0111/8735/5711/products/alaska-red-poppy-flower_720x.jpg?v=1571682569" ,
+        description: Faker::Hipster.words 
+    )
+end
+
+5.times do 
+    Bouquet.create!(
+        note: "Write note here"
+    )
+end
+
+
+
+puts "Done!"
