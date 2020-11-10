d57b6e9344d813d27bbc5c6622cd021a62d7435d
github username <github email address>
2020-11-10 12:34:46 -0500

    New seed data

--- app/serializers/bouquet_serializer.rb
+++ app/serializers/bouquet_serializer.rb
@@ -1,4 +1,3 @@
 class BouquetSerializer < ActiveModel::Serializer
   attributes :id, :note
-  has_many :flowers
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,63 @@
 
 puts "creating new records"
 
-10.times do
-    flower = Flower.create!(
-        name: Faker::Artist.name,
-        image_url: "https://cdn.shopify.com/s/files/1/0111/8735/5711/products/alaska-red-poppy-flower_720x.jpg?v=1571682569" ,
-        description: Faker::Cosmere.spren
-    )
-end
+# 10.times do
+#     flower = Flower.create!(
+#         name: Faker::Artist.name,
+#         image_url: "https://cdn.shopify.com/s/files/1/0111/8735/5711/products/alaska-red-poppy-flower_720x.jpg?v=1571682569" ,
+#         description: Faker::Cosmere.spren
+#     )
+# end
+
+Flower.create(
+    name: "Amaryllis",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Amaryllis_belladonna_flowers.jpg",
+    description: "Pride in hard won success, particularly in artistic endeavors"
+)
+
+Flower.create(
+    name: "Ambrosia",
+    image_url: "https://bluestoneperennials.global.ssl.fastly.net/img/AGKA/650/AGKA_0_Agastache_Kudos_Ambrosia3_tn.1490716087.jpg",
+    description: "Your love is reciprocated"
+)
+Flower.create(
+    name: "Yellow Carnations",
+    image_url: "https://dy1yydbfzm05w.cloudfront.net/media/catalog/product/cache/39b52c4cabb46819553175347e38b212/2/0/2074_carnation.paleblonde.cut_car_nul_yel_prd_001_1.jpg",
+    description: "Rejection, disdain, disappointment"
+)
+Flower.create(
+    name: "Pink Carnation",
+    image_url: "https://dy1yydbfzm05w.cloudfront.net/media/catalog/product/cache/39b52c4cabb46819553175347e38b212/1/1/117732_carnation.pinklemonade.cut_car_nul_plp_prd_001_1.jpg",
+    description: "Pure love: I'll never forget you"
+)
+Flower.create(
+    name: "Elder Flower",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/8/81/Sambucus_nigra0.jpg",
+    description: "Compassion"
+)
+Flower.create(
+    name: "Fern",
+    image_url: "https://cdn.shopify.com/s/files/1/1334/4597/products/PSF618-GR-Green-Forest-Fern.jpg?v=1592314727",
+    description: "Magic, enchantment, confidence, sincerity & shelter"
+)
+Flower.create(
+    name: "Hyssop",
+    image_url: "https://cdn.gardenista.com/wp-content/uploads/2018/02/hyssop-hyssopus-officinalis-crocus-uk.jpg",
+    description: "Sacrifice, cleanliness"
+)
+Flower.create(
+    name: "Blue Iris",
+    image_url: "https://dy1yydbfzm05w.cloudfront.net/media/catalog/product/cache/30e34150f80e7bd709a4ae328e3186a9/5/8/5867_iris.blueiris.irispurple_fin_3.jpg",
+    description: "Faith and hope"
+)
+
+
+
+# Flower.create(
+#     name: "",
+#     image_url: "",
+#     description: ""
+# )
 
 10.times do 
     Bouquet.create!(
